35ac3a8236f255c6eeabf58b35691f98ebb24b54
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2024-04-05 14:06:17 +0200

    added svelte stuff

---
+++ frontend/README.md
@@ -0,0 +1,19 @@
+## Developing
+
+Once you've installed dependencies with `npm install`, start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<!-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    
+</nav> -->
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">MK-Plugin-Repo</a>
+    </div>
+    <div class="flex-none gap-2">
+      <div class="form-control">
+        <!-- <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" /> -->
+        <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Search" />
+            <kbd class="kbd kbd-sm">⌘</kbd>
+            <kbd class="kbd kbd-sm">K</kbd>
+          </label>
+      </div>
+      <!-- <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <a class="justify-between">
+              Profile
+              <span class="badge">New</span>
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div> -->
+    </div>
+  </div>
+
+<h1 class="text-3xl text-red-600 font-bold underline">
+    Plugins here
+</h1>
