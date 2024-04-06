b07266870299563312535c7776fc7bfc00a53400
Leah <kevinlukej@gmail.com>
2024-04-06 02:23:49 +0200

    added the submit plugin page... or at least the start of it

---
+++ frontend/src/routes/submit/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<Navbar></Navbar>
+
+<div class="flex justify-center items-center">
+  <!-- grid-cols-3 -->
+  <div class="flex">
+    
+  </div>
+</div>
